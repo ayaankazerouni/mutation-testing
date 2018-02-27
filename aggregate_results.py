d85a98aa6a998033e5795502f073cc9a263b5fdf
Ayaan Kazerouni <ayaan@vt.edu>
2018-02-27 18:19:06 -0500

    Steps toward putting together mutation data.

---
+++ aggregate_results.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+
+import pandas as pd
+from os import listdir, path
+import argparse
+
+def __get_mutation_coverage(resultspath):
+    names = [ 'fileName', 'className', 'mutator', 'method', 'lineNumber', \
+            'status', 'killingTest' ]
+    mutations = pd.read_csv(resultspath, names=names)
+    if len(mutations) > 0:
+        nkilled = len(mutations[mutations['status'] == 'KILLED'])
+        nsurvived = len(mutations) - nkilled
+        coverage = nkilled / len(mutations)
+
+
+        return pd.Series({
+            'mutants': len(mutations),
+            'survived': nsurvived,
+            'killed': nkilled,
+            'coverage': coverage
+        })
+    else:
+        return None 
+
+def aggregate_mutation_results(infile):
+    """
+    Aggregate output from mutation testing by reading
+    results from PIT tests.
+
+    Keyword arguments:
+    infile -- Path to the directory containing tested projects
+    """
+    if (not path.isabs(infile)):
+        infile = path.abspath(infile)
+    
+    projects = listdir(infile)
+    resultpaths = {}
+
+    for proj in projects:
+        projpath = path.join(infile, proj)
+        mutationscsv = path.join(projpath, 'pitReports', 'mutations.csv')
+        mutationshtml = path.join(projpath, 'pitReports', 'com.example')
+        
+        if path.isfile(mutationscsv) and path.isdir(mutationshtml):
+            resultpaths[proj] = mutationscsv 
+
+    mutationcoverage = pd.Series(resultpaths).apply(__get_mutation_coverage)
+    return mutationcoverage
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input', help='Path to PIT testing output')
+    parser.add_argument('output', help='Path to output CSV file')
+    args = parser.parse_args()
+    
+    infile = args.input
+    outfile = args.output
+
+    results = aggregate_mutation_results(infile)
+    results.to_csv(path_or_buf=outfile, index=True)
--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,5 @@
-taskFile=$1
-project_dir="/home/ayaan/Developer/student-projects/f16-p1-submissions/"
+taskFile="tasks.json"
+project_dir="/home/ayaan/Developer/student-projects/f16-p2-submissions/"
 
 find  ${project_dir} -maxdepth 1 -type d | while read line; do
  if [[ $line != $project_dir ]]; then
