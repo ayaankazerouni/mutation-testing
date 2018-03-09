615edf7875fa8ee56291417cc89a44c9343c5f2d
Ayaan Kazerouni <ayaan@vt.edu>
2018-03-09 02:22:54 -0500

    Fixes for projects with named packages.

--- run-all.sh
+++ run-all.sh
@@ -1,15 +1,15 @@
 taskFile="tasks.json"
-project_dir="/home/ayaan/Developer/student-projects/f16-p2-submissions/"
+project_dir="/home/ayaan/Developer/student-projects/f16-p1-submissions/"
+skip_tasks=false
+skip_run=false
 
-while getopts ":t" opt; do
+while getopts ":t:r" opt; do
   case $opt in
     t)
-      find  ${project_dir} -maxdepth 1 -type d | while read line; do
-       if [[ $line != $project_dir ]]; then
-         echo "{ \"projectPath\": \"$line\", \"task\": \"pit\" }" >> $taskFile
-       fi
-      done
-      echo "Wrote tasks to ${taskFile}. Starting parallel mutation testing."
+      skip_tasks=true
+      ;;
+    r)
+      skip_run=true
       ;;
     \?)
       echo "invalid option -$OPTARG" >&2
@@ -17,8 +17,19 @@ while getopts ":t" opt; do
   esac
 done
 
-./lib/distributed-map.pl \
-  --cluster clusters.json \
-  --workScript /home/ayaan/Developer/mutation-testing/run-mutation-test.js \
-  --taskFile $taskFile \
-  --env 'PATH=/home/ayaan/.nvm/versions/node/v8.9.4/bin/node:$PATH'
+if [ "$skip_tasks" = false ] ; then 
+  find  ${project_dir} -maxdepth 1 -type d | while read line; do
+    if [[ $line != $project_dir ]]; then
+      echo "{ \"projectPath\": \"$line\", \"task\": \"pit\" }" >> $taskFile
+    fi
+  done
+  echo "Wrote tasks to ${taskFile}. Starting parallel mutation testing."
+fi
+
+if [ "$skip_run" = false ] ; then
+  ./lib/distributed-map.pl \
+    --cluster clusters.json \
+    --workScript /home/ayaan/Developer/mutation-testing/run-mutation-test.js \
+    --taskFile $taskFile \
+    --env 'PATH=/home/ayaan/.nvm/versions/node/v8.9.4/bin/node:$PATH'
+fi 
--- run-mutation-test.js
+++ run-mutation-test.js
@@ -21,6 +21,7 @@ const path = require('path');
 const fs = require('fs-extra');
 const promisify = require('util-promisify');
 const exec = promisify(require('child_process').exec);
+const traverseList = promisify(require('fs-tree-traverse').list);
 
 const argFile = process.argv[2];
 if (!argFile) {
@@ -41,25 +42,51 @@ function testSingleProject(projectPath) {
   const pkg = path.join(src, 'com', 'example');
 
   fs.copy(projectPath, clonePath)
-  // Create artificial package structure so PIT doesn't try to mutate itself
-  // .then(() => {
-  //   // Create src/com/example
-  //   return fs.mkdirp(pkg);
-  // })
-  // .then(() => {
-  //   // Move Java files into src/com/example
-  //   return exec(`mv ${path.join(src, '*.java')} ${pkg}`);
-  // })
-  // .then(() => {
-  //   // Add the package declaration to the top of Java files
-  //   return exec(`sed -i '1ipackage com.example;' ${path.join(pkg, '*.java')}`);
-  // })
+  // Create artificial package structure for items in the default package
+  // so PIT doesn't try to mutate itself
   .then(() => {
+    // Create src/com/example
+    return fs.mkdirp(pkg);
+  })
+  .then(() => {
+    // Move Java files into src/com/example
+    return exec(`mv ${path.join(src, '*.java')} ${pkg}`);
+  })
+  .then(() => {
+    // Add the package declaration to the top of Java files
+    return exec(`sed -i '1ipackage com.example;' ${path.join(pkg, '*.java')}`);
+  })
+  .then(() => {
+    return traverseList(src, { relative: true } );
+  })
+  .then((dirTree) => {
+    // Compute packages to point PIT at
+    var dirs = dirTree.map(p => path.dirname(p)); 
+    var targetClasses = [];
+    var targetTests = [];
+    
+    dirs = [...new Set(dirs)]; // unique 
+    dirs.forEach(d => {
+      d = d.replace("/", ".");
+      var packageName;
+      if (d.endsWith(".")) {
+        packageName = `${d}*`; 
+      } else {
+        packageName = `${d}.*`; 
+      }
+
+      targetClasses.push(packageName);
+      targetTests.push(`${packageName}Test*`);
+    })
+
+    targetClasses = targetClasses.join(",");
+    targetTests = targetTests.join(",");
+
     // run ANT
     const antPath = path.join(__dirname, 'build.xml'); // path to shared build file
     const libPath = path.join(__dirname, 'lib'); // path to shared libraries 
 
-    return exec(`ant -f ${antPath} -Dbasedir=${clonePath} -Dresource_dir=${libPath} ${task}`); 
+    return exec(`ant -f ${antPath} -Dbasedir=${clonePath} -Dresource_dir=${libPath} -Dtarget_classes=${targetClasses} -Dtarget_tests=${targetTests} ${task}`); 
   })
   .then((result) => { // this is an object
     if (!suppressOut) console.log(`stdout: ${result.stdout.toString()}`);
