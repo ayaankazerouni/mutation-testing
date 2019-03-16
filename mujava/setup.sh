848934a88ae623fde6ce42b8528d11c77fbef713
Ayaan Kazerouni <ayaan@vt.edu>
2019-03-16 00:01:32 -0400

    Fix dependency scripts

--- mujava/setup.sh
+++ mujava/setup.sh
@@ -1,8 +1,9 @@
 mkdir lib
 
-curl -H "Accept: application/zip" -o lib/student.jar http://sourceforge.net/projects/web-cat/files/Student%20Library/4.14/student.jar/download
-curl -H "Accept: application/zip" -o lib/mujava.jar https://cs.gmu.edu/~offutt/mujava/mujava.jar
-curl -H "Accept: application/zip" -o lib/openjava.jar https://cs.gmu.edu/~offutt/mujava/openjava.jar
-curl -H "Accept: application/zip" -o lib/junit.jar http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar
-curl -H "Accept: application/zip" -o lib/commons-io.jar https://cs.gmu.edu/~offutt/mujava/muscript/commons-io-2.4.jar
-curl -H "Accept: application/zip" -o lib/tools.jar http://maven.jahia.org/maven2/com/sun/tools/1.5.0/tools-1.5.0.jar
+curl -o lib/mujava.jar https://cs.gmu.edu/~offutt/mujava/mujava.jar
+curl -o lib/openjava.jar https://cs.gmu.edu/~offutt/mujava/openjava.jar
+curl -o lib/junit.jar http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar
+curl -o lib/commons-io.jar https://cs.gmu.edu/~offutt/mujava/muscript/commons-io-2.4.jar
+curl -o lib/tools.jar http://maven.jahia.org/maven2/com/sun/tools/1.5.0/tools-1.5.0.jar
+
+echo -e "\n\nstudent.jar not downloaded! Download it manually from the following link\nhttp://sourceforge.net/projects/web-cat/files/Student%20Library/4.14/student.jar/download"
