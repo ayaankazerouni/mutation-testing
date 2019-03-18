679433fd4fdcbd2acee5ed915d09fbd035024ffd
Ayaan Kazerouni <ayaan@vt.edu>
2019-03-18 14:08:38 -0400

    Add deps

--- mujava/setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir lib
-
-curl -o lib/mujava.jar https://cs.gmu.edu/~offutt/mujava/mujava.jar
-curl -o lib/openjava.jar https://cs.gmu.edu/~offutt/mujava/openjava.jar
-curl -o lib/junit.jar http://central.maven.org/maven2/junit/junit/4.12/junit-4.12.jar
-curl -o lib/commons-io.jar https://cs.gmu.edu/~offutt/mujava/muscript/commons-io-2.4.jar
-curl -o lib/tools.jar http://maven.jahia.org/maven2/com/sun/tools/1.5.0/tools-1.5.0.jar
-
-echo -e "\n\nstudent.jar not downloaded! Download it manually from the following link\nhttp://sourceforge.net/projects/web-cat/files/Student%20Library/4.14/student.jar/download"
