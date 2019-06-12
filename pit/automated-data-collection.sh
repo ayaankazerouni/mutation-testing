e7decd248b9198e8fce1b706c81d0c0854d1692e
Arinjoy-Basak <basakarinjoy@gmail.com>
2019-06-12 13:55:17 -0400

    corrected tar directory depth in automated script; changed gitignore

--- pit/automated-data-collection.sh
+++ pit/automated-data-collection.sh
@@ -1,18 +1,9 @@
-#Automation lines for a whole semester's project
-#for projdir in /home/mutant/DATA/submissions/p1 \
-#	/home/mutant/DATA/submissions/p2 \
-#	/home/mutant/DATA/submissions/p3 \
-#	/home/mutant/DATA/submissions/p4
-#do
-#	[ -d $projdir ] && echo "Directory $projdir exists."	
-#done
-
 inputdir=$1 #Full path to original folder
 outputdirname=$2 #path starting in outputs; such as fall2018/p1
 
 #Sanity check; change the directory to parent of pit/
 cd ~/code/forked/mutation-testing
-./write_tasks.py $1 -n 2 > pit/tasks.ndjson
+./write_tasks.py $1 -n 1 > pit/tasks.ndjson
 
 #Change directory to pit/ and run docker container
 cd pit/
@@ -22,7 +13,8 @@ cd pit/
 mkdir -p $2
 cd /tmp/mutation-testing/
 find . -name 'mutations.csv' -exec cp --parents \{\} $2 \;
-cp ~/code/forked/mutation-testing/pit/mutation-results.ndjson $2
 
-tar -czvf $2.tar.gz $2
+mv ~/code/forked/mutation-testing/pit/mutation-results.ndjson $2
+
+tar -czvf $2.tar.gz -C $2 ../
 rm -rf $2
