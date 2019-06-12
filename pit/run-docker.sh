21ee19d29cbef8d4021124cfcfe6ea03391f71c0
Arinjoy-Basak <basakarinjoy@gmail.com>
2019-06-12 16:26:06 -0400

    changes made for pitesting cs2114 - part 1

--- pit/run-docker.sh
+++ pit/run-docker.sh
@@ -13,7 +13,8 @@ esac
 [[ -z "$taskfile" ]] && echo 'Missing task file argument (-t|--tasks)' && exit 1
 
 [[ -f $taskfile ]] && \
-  ../clone-projects.py $taskfile -p &&
+  #../clone-projects.py $taskfile -p &&
+  ../clone-projects.py $taskfile &&
   docker build \
     --build-arg TASKFILE=$taskfile \
     --build-arg CLONEDIR=$clonedir \
